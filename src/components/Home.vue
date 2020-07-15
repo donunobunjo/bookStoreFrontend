27df58ed527ea3e99c9d420134d7a9bd4a26a27a
donunobunjo <don.unobunjo@gmail.com>
2020-07-15 15:13:58 +0100

    Refactor: the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,44 @@
         <div class="text-center" style="margin: 20px 0px 20px 0px;">
             <span class="text-secondary">Wellcome to the bookstore</span>
         </div>
-
+        <table class="table table-responsive">
+                    <thead>
+                        <tr>
+                            <th>Isbn</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Review</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="book in books" :key="book.id">
+                            <td>{{book.isbn}}</td>
+                            <td>{{book.title}}</td>
+                            <td>{{book.description}}</td>
+                            <td>{{book.review}}</td>
+                            
+                        </tr>
+                        
+                    </tbody>
+                </table>
         
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+      data(){
+          return{
+              books:[],
+          }
+      },
+      mounted(){
+          this.axios
+            .get('http://localhost:8000/api/books')
+            .then((res)=>{
+                this.books = res.data
+            })
+      } 
+    }
 </script>
